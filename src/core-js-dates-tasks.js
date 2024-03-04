866473617ac0bac31dfdd60be82c8e6deb43fe4d
Jerzy Matusewicz <vfnectdbx78@gmail.com>
2024-03-04 17:52:46 +0400

    init: my first commit

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,9 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const myTimesTamp = new Date(date);
+  return myTimesTamp.getTime();
 }
 
 /**
@@ -31,8 +32,14 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const timeArrey = [hours, minutes, seconds];
+  const dateArr = timeArrey.map((num) => `${num < 10 ? `0${num}` : num}`);
+  const onlyTime = dateArr.join(':');
+  return onlyTime;
 }
 
 /**
@@ -46,8 +53,21 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dateOk = new Date(date);
+  const myFavoriteDay = dateOk.getUTCDay();
+
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  return daysOfWeek[myFavoriteDay];
 }
 
 /**
