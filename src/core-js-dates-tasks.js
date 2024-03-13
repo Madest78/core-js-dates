34ca2d6d7e7188d93ba15244a2c30da897324f05
Jerzy Matusewicz <vfnectdbx78@gmail.com>
2024-03-13 22:34:14 +0400

    daa new solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -81,8 +81,17 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const randomData = new Date(date);
+  const randomDay = randomData.getUTCDay();
+  const friday = 5;
+
+  let daysToAdd = friday - randomDay;
+  if (daysToAdd <= 0) {
+    daysToAdd += 7;
+  }
+  randomData.setUTCDate(randomData.getUTCDate() + daysToAdd);
+  return randomData;
 }
 
 /**
@@ -96,8 +105,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month).getUTCDate();
 }
 
 /**
@@ -111,8 +120,15 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const starsData = new Date(dateStart);
+  const endData = new Date(dateEnd);
+
+  const difference = endData.getTime() - starsData.getTime();
+
+  const daysDifference = Math.floor(difference / (1000 * 3600 * 24));
+
+  return daysDifference + 1;
 }
 
 /**
@@ -132,8 +148,10 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const givenDate = new Date(date);
+  const { start, end } = period;
+  return givenDate >= new Date(start) && givenDate <= new Date(end);
 }
 
 /**
@@ -147,8 +165,23 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function padZero(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date) {
+  const currentDate = new Date(date);
+  const localOffsetHours = -4; // Georgia
+  currentDate.setHours(currentDate.getHours() + localOffsetHours);
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const second = currentDate.getSeconds();
+  const period = hour < 12 ? 'AM' : 'PM';
+  const formattedHour = hour % 12 || 12;
+  return `${month}/${day}/${year}, ${formattedHour}:${padZero(minute)}:${padZero(second)} ${period}`;
 }
 
 /**
